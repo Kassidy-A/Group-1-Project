e48b6aac9943a92729e5536c2bff8768a01ef5e6
ACE251 <cfuller197001@troy.edu>
2025-04-28 12:17:04 -0500

    Update Group Project.py

--- Group Project.py
+++ Group Project.py
@@ -19,16 +19,43 @@ import matplotlib.pyplot as plt
 df = pd.read_csv()
 
 
+# Displaying the first few rows
+print(df.head())
+print("\n Data Info\n")
+print(df.info())
+
+
+print("\nMissing Values: ")
 df.isnull().sum()      #checks for missing values
+print(df.isnull().sum())
+
+
+# check dataset shape
+print(f"Dataset shapr {}")
+
+
 df.dropna(inplace=True)     #drops missing values
-df.duplicated().sum       #checks for duplicate data
+
+
+duplicate_row = df.duplicated().sum       #checks for duplicate data
+print(f"Number of duplication row {duplicate_row}")
+if duplicate_row > 0
+    df = df.drop_duplication()
 df.drop_duplicates(inplace=True)  
+
+
+# Basic cleaning: removing leading/trailing spaces from column names
+df.column = df.column.str.strip()
 
 ###############################################################################
 
 # Step 3: Summary Stats, Descriptive Stats, and Correlation
 
 #one line of code 
+
+summary_stats = df.describe()
+print("\n Summary Statistics: ")
+print(summary_stats)
 #df.describe()
 #what do these stats tell you?
 
